27e3fd0d969a1bc9b5c1e7ec24d1952f83d3db48
Alesya Sheremet <alesya.sheremet@gmail.com>
2025-06-12 22:13:18 +0200

    .

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+  <style>
+    html, body {
+      margin: 0;
+      padding: 0;
+      height: 100%;
+    }
+
+    body {
+      background-image: url('assets/Image003.jpg'); /* Replace with your image path */
+      background-size: cover;      /* Ensures the image covers the entire page */
+      background-repeat: no-repeat; /* Prevents tiling */
+      background-position: center;  /* Centers the image */
+      background-attachment: fixed; /* Optional: Makes the image stay fixed during scroll */
+    }
+  </style>
